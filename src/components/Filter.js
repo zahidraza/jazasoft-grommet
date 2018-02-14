2ea0db72799b2d25fc36ff1e519ac93c90c3f398
Md Zahid Raza <zahid7292@ymail.com>
2018-02-15 00:01:33 +0530

    missed changes of previous commit

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import PropTypes, { element } from 'prop-types';
+import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 
 import {splitCamelCase} from '../utils/utility';
